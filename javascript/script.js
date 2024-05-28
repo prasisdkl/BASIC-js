2779cd445b7a6afba6bd7b771944b706cd581c2d
prasisdkl <118652248+prasisdkl@users.noreply.github.com>
2024-05-28 22:33:13 +0545

    js html

---
+++ javascript/script.js
@@ -0,0 +1,104 @@
+
+
+/*
+document.write("<h1>me BASIC javascript</h1>");
+*/
+
+
+function showdate(){
+    document.getElementById('datep').innerHTML = Date();
+}
+
+
+
+/*
+let a = 2;
+ let b =5;
+ let sum = a+b;
+ let mul = a*b;
+ let dive = a/b;
+ let mod = a%b;
+
+ document.write(sum);
+ alert("i am hacker");
+
+ console.log(area);
+ */ 
+
+
+/*
+const pi =3.14;
+let r = 45;
+
+let area = pi * r*r;
+
+document.write(area.toFixed(3));
+*/
+
+/*
+let fname = "Praasis";
+let lname = "Dhakal";
+document.write(fname +" "+lname);
+*/
+
+/*
+let myage = 17;
+ 
+if(myage > 18)
+    {
+    document.write("you are voter");
+}
+else
+{
+    document.write(" you are not voter")
+}
+*/
+
+
+/*
+let myage = 18;
+ 
+if(myage > 18)
+    {
+    document.write("you are voter");
+}else if(myage == 18)
+    {
+        document.write("Age Is Equal To 18");
+    }
+else
+{
+    document.write(" you are not voter");
+}
+*/
+
+
+//looping practice
+
+//for loop
+
+/*
+for(let i=1; i<=100; i++){
+    document.write("<br>"+i);
+}
+
+for(let i=1; i<=100; i++){
+    document.write("prasis<br>");
+}
+*/
+
+//while loop
+/*
+let i =0;
+while(i<=100){
+        document.write("<br>"+i);
+        i++;
+}
+*/
+
+
+//do while loop
+let a =0;
+do{
+    document.write(+a);
+    a++
+}while(a<=100);
